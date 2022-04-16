efd57ab4ee97b36738f35d67de412b57ab88bc89
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-04-16 14:40:13 -0500

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,19 +1,17 @@
 import markovify
 
 # Get raw text as string.
-with open("turrets.txt") as f:
+with open("name of text file.txt") as f:
     text = f.read()
 
+# How many words it looks ahead to decide next word.
+complexity = 2
+    
+    
 # Build the model.
-text_model_turrets = markovify.Text(text, state_size = 2)
+text_model = markovify.Text(text, state_size = Compexity)
 f.close()
-
-with open("units.txt") as f:
-    text = f.read()
-text_model_units = markovify.Text(text, state_size = 2)
-
-model_combo = markovify.combine([ text_model_turrets, text_model_units ], [ 1, 1 ])
 
 for i in range(5):
     a  = model_combo.make_sentence()
-    print(a.replace(' ', ''))
+    print(a)
